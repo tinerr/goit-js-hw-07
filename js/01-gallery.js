c4597dbceda04f51feb3a9d39bc6901c6541288c
tinerr <tinerr02@gmail.com>
2023-12-12 19:32:20 +0800

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGallery = (el) => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+};
+
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", photosMarkup);
+
+//-----------------------------------------------------------------
+const handleGalleryClick = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const urlOriginal = event.target.dataset.source;
+
+  //create new basickLightBox instance
+
+  const instance = basicLightbox.create(`<img src="${urlOriginal}">`);
+
+  instance.show();
+
+  // handleOnEscKeyPress
+
+  const handleOnEscKeyPress = (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+      window.addEventListener("keydown", handleOnEscKeyPress);
+    }
+  };
+
+  window.addEventListener("keydown", handleOnEscKeyPress);
+};
+galleryList.addEventListener("click", handleGalleryClick);
